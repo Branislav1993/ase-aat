025d60dbf77bfc0eb368eeabd68d2262adcff85e
NaumGj <naum_gj@hotmail.com>
2017-01-02 22:03:35 +0100

    Sign in and register screens added

---
+++ app/src/main/java/de/tum/aat/ExerciseListActivity.java
@@ -0,0 +1,81 @@
+package de.tum.aat;
+
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.NotificationCompat;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class ExerciseListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercise_list);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            NotificationCompat.Builder mBuilder =
+                    (NotificationCompat.Builder) new NotificationCompat.Builder(this)
+                            .setSmallIcon(R.drawable.ic_ase)
+                            .setContentTitle("My notification")
+                            .setContentText("Hello World!");
+            // Creates an explicit intent for an Activity in your app
+//            Intent resultIntent = new Intent(this, ResultActivity.class);
+//
+//            // The stack builder object will contain an artificial back stack for the
+//            // started Activity.
+//            // This ensures that navigating backward from the Activity leads out of
+//            // your application to the Home screen.
+//            TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+//            // Adds the back stack for the Intent (but not the Intent itself)
+//            stackBuilder.addParentStack(ResultActivity.class);
+//            // Adds the Intent that starts the Activity to the top of the stack
+//            stackBuilder.addNextIntent(resultIntent);
+//            PendingIntent resultPendingIntent =
+//                    stackBuilder.getPendingIntent(
+//                            0,
+//                            PendingIntent.FLAG_UPDATE_CURRENT
+//                    );
+//            mBuilder.setContentIntent(resultPendingIntent);
+            NotificationManager mNotificationManager =
+                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            // mId allows you to update the notification later on.
+            mNotificationManager.notify(1, mBuilder.build());
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
