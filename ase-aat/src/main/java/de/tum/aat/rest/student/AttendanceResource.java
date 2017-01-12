7cb6178508e8eb701ad5a18f3b1a2d787125e1f9
Branislav Vidojevic <branislav.vidojevic@msg-global.com>
2017-01-12 22:04:18 +0100

    Changed QR code prestenation to JSON, url parameter is used

--- ase-aat/src/main/java/de/tum/aat/rest/student/AttendanceResource.java
+++ ase-aat/src/main/java/de/tum/aat/rest/student/AttendanceResource.java
@@ -1,22 +1,17 @@
 package de.tum.aat.rest.student;
 
-import org.restlet.data.MediaType;
-import org.restlet.representation.ByteArrayRepresentation;
-import org.restlet.representation.Representation;
 import org.restlet.resource.Get;
 import org.restlet.resource.ServerResource;
 
 import de.tum.aat.domain.Student;
 import de.tum.aat.exceptions.NotFoundException;
 import de.tum.aat.logic.QRGenerator;
-import net.glxn.qrgen.QRCode;
-import net.glxn.qrgen.image.ImageType;
 
 public class AttendanceResource extends ServerResource {
 
 	private static final QRGenerator QR = new QRGenerator();
 
-	@Get("plain/text")
+	@Get("json")
 	public String getAttendanceQR() {
 
 		long id = Long.MIN_VALUE;
@@ -27,14 +22,17 @@ public class AttendanceResource extends ServerResource {
 			throw new NotFoundException(Student.class);
 		}
 
-//		byte[] data = QRCode.from(QR.generateAttendance(id)).to(ImageType.PNG).stream().toByteArray();
-//
-//		ByteArrayRepresentation bar = new ByteArrayRepresentation(data, MediaType.IMAGE_PNG);
-//		getResponse().setEntity(bar);
-//
-//		return bar;
-		
-		return QR.generateAttendance(id);
+		// byte[] data =
+		// QRCode.from(QR.generateAttendance(id)).to(ImageType.PNG).stream().toByteArray();
+		//
+		// ByteArrayRepresentation bar = new ByteArrayRepresentation(data,
+		// MediaType.IMAGE_PNG);
+		// getResponse().setEntity(bar);
+		//
+		// return bar;
+
+		return "{\"url\":\"" + QR.generateAttendance(id) + "\"}";
+
 	}
 
 }
--- ase-aat/src/main/java/de/tum/aat/rest/student/PresentationResource.java
+++ ase-aat/src/main/java/de/tum/aat/rest/student/PresentationResource.java
@@ -1,23 +1,18 @@
 package de.tum.aat.rest.student;
 
-import org.restlet.data.MediaType;
-import org.restlet.representation.ByteArrayRepresentation;
-import org.restlet.representation.Representation;
 import org.restlet.resource.Get;
 import org.restlet.resource.ServerResource;
 
 import de.tum.aat.domain.Student;
 import de.tum.aat.exceptions.NotFoundException;
 import de.tum.aat.logic.QRGenerator;
-import net.glxn.qrgen.QRCode;
-import net.glxn.qrgen.image.ImageType;
 
 public class PresentationResource extends ServerResource {
 
 	private static final QRGenerator QR = new QRGenerator();
 
-	@Get("image/png")
-	public Representation getPresentationQR() {
+	@Get("json")
+	public String getPresentationQR() {
 
 		long id = Long.MIN_VALUE;
 
@@ -27,12 +22,16 @@ public class PresentationResource extends ServerResource {
 			throw new NotFoundException(Student.class);
 		}
 
-		byte[] data = QRCode.from(QR.generatePresentation(id)).to(ImageType.PNG).stream().toByteArray();
+		// byte[] data =
+		// QRCode.from(QR.generatePresentation(id)).to(ImageType.PNG).stream().toByteArray();
+		//
+		// ByteArrayRepresentation bar = new ByteArrayRepresentation(data,
+		// MediaType.IMAGE_PNG);
+		// getResponse().setEntity(bar);
 
-		ByteArrayRepresentation bar = new ByteArrayRepresentation(data, MediaType.IMAGE_PNG);
-		getResponse().setEntity(bar);
+		// return bar;
 
-		return bar;
+		return "{\"url\":\"" + QR.generatePresentation(id) + "\"}";
 	}
 
 }
